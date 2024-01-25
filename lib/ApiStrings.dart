a63ed6a5371c8112308f4f2acc4198d098cc378e
SUJAY-CSG <sujay.kumar@global-csg.com>
2024-01-25 10:23:46 +0530

    created activity flow final to showcase demo

--- lib/ApiStrings.dart
+++ lib/ApiStrings.dart
@@ -4,4 +4,15 @@ class ApiStrings {
   static const String endpoint_create_activity = '/activity/add-activity';
   static const String endpoint_update_activity = '/activity/update-activity';
   static const String endpoint_get_all_activity = '/activity/get-all-activities';
+
+
+  //create Activity field
+static const String endpoint_get_all_service_techs = '/service-tech/get-all-service-techs';
+  static const String endpoint_get_all_rail_location = '/rail-location/get-all-rail-locations';
+  static const String endpoint_get_all_helpers = '/helper/get-all-helpers';
+  static const String endpoint_get_all_activity_types = '/activity-type/get-all-activity-types';
+  static const String endpoint_get_all_activity_status = '/activity-status/get-all-activity-status';
+  // static const String endpoint_get_all_ = '';
+  // static const String endpoint_get_all_ = '';
+  // static const String endpoint_get_all_ = '';
 }
